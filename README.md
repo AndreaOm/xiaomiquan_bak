d80f7cd0e9f8f8123d29c141817f6df70f8c1e4f
z0z <472354643@qq.com>
2017-08-25 18:54:19 +0800

    share script

--- README.md
+++ README.md
@@ -1,5 +1,44 @@
 # 小密圈备份
-目前备份了
+
+为了避免再次发生`与其相信谣言，不如相信XXX`的悲剧
+
+---
+
+目前备份了 	20170824
 1. `代码审计-2212251881`
 2. `qz安全情报分析-1852214142`
 3. `安全数据与机器学习-5421844144`
+
+---
+
+后面不再push备份的json
+如有需要修改 xiaomiquan_bak.py 中的 config 备份自己想要的圈子
+
+```
+token
+name
+user_id
+avatar_url
+```
+
+--- 
+
+## Requirement
+
+* `python 2.7`
+* `pip install requests`
+
+--- 
+
+## Usage
+
+### step 1 `网页端登录后获取cookies`
+![step 1](https://ws2.sinaimg.cn/large/a15b4afegy1fiw5vsqcocj211g09cmye.jpg)
+
+### step 2 `修改 xiaomiquan_bak.py`
+![step 2](https://ws2.sinaimg.cn/large/a15b4afegy1fiw5xfydk9j20gj029749.jpg)
+
+### step 3 `设置需要备份的圈子ID`
+![step 3](https://ws2.sinaimg.cn/large/a15b4afegy1fiw5y83q35j20a0028a9w.jpg)
+
+### step 4 `python xiaomiquan_bak.py`
---
+++ xiaomiquan_bak.py
@@ -0,0 +1,68 @@
+import json
+import time
+import requests
+from urllib import quote
+
+token = '84E9416F-9B3A-73B7-4966-AEA7FB10B729' # Your access_token
+name = 'xxx' # Your name
+user_id = '0000000000000' # Your user_id
+avatar_url = 'https://file.xiaomiquan.com/201612/xxx.jpg' # Your avatar_url
+
+
+headers = {
+    'Host': 'wapi.xiaomiquan.com',
+    'Origin': 'https://wx.xiaomiquan.com',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+    'authorization': token,
+    'Accept': '*/*',
+    'Referer': 'https://wx.xiaomiquan.com/dweb/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.'+str(time.strftime('%S',time.localtime(time.time()))),
+    'Connection': 'keep-alive',
+    'x-request-id': '92c35538-be75-3029-4d8a-e338e6'+str(time.strftime('%H%M%S',time.localtime(time.time())))
+}
+
+cookies = {
+    'access_token': token,
+    'name': name,
+    'user_id': user_id,
+    'PHPSESSID': '42g23c6ss7140joq3mmsb114t2',
+    'UM_distinctid': '15d59be81a811b-0e2bd99982822d8-7f682331-1fa400-15d59be81a930f',
+    'ws_address': 'ws_address=wss%3A//ws.xiaomiquan.com%3A443/ws%3Fversion%3Dv1.6%26access_token%3D'+token,
+    'avatar_url': avatar_url
+}
+
+
+api = 'https://wapi.xiaomiquan.com/v1.6/'
+
+
+def saveTopics(groupId):
+    o = getAllTopics(groupId,getTimeNow())
+    with open(str(time.strftime('%Y%m%d',time.localtime(time.time()))) + '-' + str(groupId) + '.json','wb+') as f:
+        f.write(json.dumps(o))
+
+def getTopicsByTime(groupId,t):
+    time.sleep(1)
+    return requests.get(api + 'groups/' + str(groupId) + '/topics?count=20&end_time=' + quote(t), headers=headers , cookies=cookies)
+
+def getEndTime(o):
+    return o['resp_data']['topics'][len(o['resp_data']['topics'])-1]['create_time']
+
+def getTimeNow():
+    return str(time.strftime('%Y-%m-%dT%H:%M:%S.6%S+0800',time.localtime(time.time())))
+
+def getAllTopics(groupId,t):
+    d = []
+    n = (getTopicsNums(groupId)/20+1)
+    for x in range(n):
+        c = getTopicsByTime(groupId, t)
+        t = getEndTime(json.loads(c.content))
+        d.extend(json.loads(c.content)['resp_data']['topics'])
+    return d
+
+def getTopicsNums(groupId):
+    return json.loads(requests.get(api + 'groups/' + str(groupId) + '/details', headers=headers , cookies=cookies).content)['resp_data']['group']['statistics']['topics']['topics_count']
+
+saveTopics(2212251881) # set bakup group id
+saveTopics(1852214142)
+saveTopics(5421844144)
